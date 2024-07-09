d331b3241f412685977d9258245d9eb848e00fea
benjamin.bajorat <benjamin.bajorat@mayflower.de>
2024-07-09 17:05:15 +0200

    implement api for cart_items table

--- src/main/java/works/weave/socks/cart/item/FoundItem.java
+++ src/main/java/works/weave/socks/cart/item/FoundItem.java
@@ -2,6 +2,7 @@ package works.weave.socks.cart.item;
 
 import org.slf4j.Logger;
 import works.weave.socks.cart.entities.Item;
+import works.weave.socks.cart.repositories.ItemRepository;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -9,29 +10,41 @@ import java.util.function.Supplier;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class FoundItem implements Supplier<Item> {
-    private final Logger LOG = getLogger(getClass());
-    private final Supplier<List<Item>> items;
-    private final Supplier<Item> item;
+  private final Logger LOG = getLogger(getClass());
+  private Supplier<Item> item;
+  private final Supplier<ItemRepository> itemRepository;
+  private int customerId;
 
-    public FoundItem(Supplier<List<Item>> items, Supplier<Item> item) {
-        this.items = items;
-        this.item = item;
-    }
+  public FoundItem(int customerId, Supplier<ItemRepository> itemRepository) {
+    this.customerId = customerId;
+    this.itemRepository = itemRepository;
+  }
 
-    @Override
-    public Item get() {
-        return items.get().stream()
-                .filter(item.get()::equals)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Cannot find item in cart"));
-    }
+  public FoundItem(Supplier<Item> item, Supplier<ItemRepository> itemRepository) {
+    this.item = item;
+    this.itemRepository = itemRepository;
+  }
 
-    public boolean hasItem() {
-        boolean present = items.get().stream()
-                .filter(item.get()::equals)
-                .findFirst()
-                .isPresent();
-        LOG.debug((present ? "Found" : "Didn't find") + " item: " + item.get() + ", in: " + items.get());
-        return present;
-    }
+  @Override
+  public Item get() {
+    return itemRepository.get().findByItemId(item.get().getItemId());
+  }
+
+  public List<Item> getByCustomerId() {
+    return itemRepository.get().findByCustomerId(customerId);
+  }
+
+  public Item getByItemId() {
+    return getByCustomerId().stream()
+                   .filter(itemEntry -> itemEntry.getItemId() == item.get().getItemId())
+                   .findFirst()
+                   .orElseThrow(() -> new IllegalArgumentException("Cannot find item in db"));
+  }
+
+  public Item getByItemId(int itemId) {
+    return getByCustomerId().stream()
+                   .filter(itemEntry -> itemEntry.getItemId() == itemId)
+                   .findFirst()
+                   .orElseThrow(() -> new IllegalArgumentException("Cannot find item in db"));
+  }
 }
--- src/main/java/works/weave/socks/cart/repositories/ItemRepository.java
+++ src/main/java/works/weave/socks/cart/repositories/ItemRepository.java
@@ -1,10 +1,31 @@
 package works.weave.socks.cart.repositories;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import works.weave.socks.cart.entities.Item;
 
-@RepositoryRestResource
-public interface ItemRepository extends MongoRepository<Item, String> {
-}
+import java.util.List;
 
+@RepositoryRestResource
+@Transactional
+public interface ItemRepository extends JpaRepository<Item, String> {
+  Item findByItemId(@Param("itemId") int itemId);
+
+  List<Item> findByCustomerId(@Param("customerId") int customerId);
+
+  @Modifying
+  @Query("UPDATE Item i SET i.quantity = ?3, i.price = ?4 WHERE i.customerId = ?1 AND i.itemId = ?2")
+  void updateAllByCustomerId(int customerId, int itemId, int quantity, float price);
+
+  @Modifying
+  @Query("DELETE FROM Item i where i.customerId = ?1 AND i.itemId = ?2")
+  void deleteByCustomerIdAndItemId(int customerId, int itemId);
+
+  @Modifying
+  @Query("DELETE FROM Item i where i.customerId = ?1")
+  void deleteByCustomerId(int customerId);
+}
